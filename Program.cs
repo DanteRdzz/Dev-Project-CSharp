636ff51794e2b95fb81e49c46c53615d45446ba6
DanteRdz <dantevegardz@gmail.com>
2025-09-15 00:28:36 -0600

    15/09/2025 - Create Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+namespace DevProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
